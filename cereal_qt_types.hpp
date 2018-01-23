742920214f00b9ae5b7c36e1ae86b5ccc7c10004
Soeren Sproessig <ssproessig@gmail.com>
2018-01-23 23:21:48 +0100

    made load_minimal/save_minimal inline as cereal code suggests

--- cereal_qt_types.hpp
+++ cereal_qt_types.hpp
@@ -6,13 +6,13 @@ namespace cereal
 {
     /// @name QDateTime serialization
     /// @{
-    template <class Archive>
+    template <class Archive> inline
     int64_t save_minimal(Archive const&, QDateTime const& aDateTime)
     {
         return aDateTime.toUTC().toSecsSinceEpoch();
     }
 
-    template <class Archive>
+    template <class Archive> inline
     void load_minimal(Archive const&, QDateTime& aDateTime, int64_t const& aValue)
     {
         aDateTime = QDateTime::fromSecsSinceEpoch(aValue, Qt::UTC);
